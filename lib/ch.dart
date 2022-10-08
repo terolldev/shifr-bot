683a45b628ac031ea0772674935bd0ac463beadf
TimEiger <slavajkvitya@gmail.com>
2022-10-08 16:54:02 +0300

    Update ch.dart

--- lib/ch.dart
+++ lib/ch.dart
@@ -1,6 +1,10 @@
 import 'package:nyxx/nyxx.dart';
 class Obj {
+<<<<<<< Updated upstream
   String TOKEN = "*********************************************************************************";
+=======
+  String TOKEN = "ODE1MzE1Mzg4MDczNjM5OTQ4.GzWtuq.1g46YTrTsV-IllpGEAXLieJBX6zCRpgCBG4Gtw";
+>>>>>>> Stashed changes
   ActivityBuilder STATUS = ActivityBuilder.watching("on birds");
   UserStatus ACT = UserStatus.idle;
   int INTENT = GatewayIntents.all;
